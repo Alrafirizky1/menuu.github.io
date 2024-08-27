adbd6224f8ed8f2635be10d2a84953e2066f0d80
Alrafi <Malrafirizky@gmail.com>
2024-08-27 16:12:47 +0700

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,17 @@
 /*==================== SHOW MENU ====================*/
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll(".home__img");
+    let currentIndex = 0;
+
+    function showNextImage() {
+        images[currentIndex].classList.remove("active");
+        currentIndex = (currentIndex + 1) % images.length;
+        images[currentIndex].classList.add("active");
+    }
+
+    setInterval(showNextImage, 5000); // Ganti gambar setiap 3 detik
+});
+
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
